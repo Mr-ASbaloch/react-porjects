ca97619ef87bf3b287b4f2be09b19bdf730535de
Mr-ASbaloch <abdulsaeed1356@gmail.com>
2023-09-03 15:40:21 +0500

    commit

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Counter from './Counter/Counter'
+
+function App() {
+  
+
+  return (
+    <div  >
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ counter-app/src/Counter/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button, Card, message } from "antd";
+
+const Counter = () => {
+  const [number, setNumber] = useState(1);
+  const increment =()=>{
+    setNumber (number+ 1)
+  }
+  const decrement =()=>{
+    setNumber (number- 1)
+    if (number < 1){
+       message.error ('sorry ! number is less than 0')
+        return setNumber(0)
+    }
+  }
+  return (
+    <div className="mt-[120px] flex justify-center w-full ">
+      <Card
+        title="Counter App"
+        bordered={true}
+        style={{
+          width: 300,
+        }}
+        className="bg-[#e7b5b5]  text-blue-800   text-2xl "
+      >
+        <Button className="text-blue-900 font-bold  mb-5 ml-[30%] " onClick={increment}>
+          {" "}
+          Increment{" "}
+        </Button>
+        <h1 className="text-center font-bold">{number}</h1>
+        <Button className="text-blue-900 font-bold mt-5  ml-[30%]" onClick={decrement}>
+          {" "}
+          Decrement{" "}
+        </Button>
+      </Card>
+    </div>
+  );
+};
+export default Counter;
